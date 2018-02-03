5ad5fa1f72014ba7e8e0112afee276e47f7a6f29
giantcroc <1204449533@qq.com>
2018-02-03 22:56:27 +0800

    update

--- assignment3/cs231n/image_utils.py
+++ assignment3/cs231n/image_utils.py
@@ -78,6 +78,24 @@ def deprocess_image(img, mean_img, mean='image', renorm=False):
     img = 255.0 * (img - low) / (high - low)
   return img.astype(np.uint8)
 
+SQUEEZENET_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+SQUEEZENET_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+
+def preprocess_image1(img):
+    """Preprocess an image for squeezenet.
+    
+    Subtracts the pixel mean and divides by the standard deviation.
+    """
+    return (img.astype(np.float32)/255.0 - SQUEEZENET_MEAN) / SQUEEZENET_STD
+
+
+def deprocess_image1(img, rescale=False):
+    """Undo preprocessing on an image and convert back to uint8."""
+    img = (img * SQUEEZENET_STD + SQUEEZENET_MEAN)
+    if rescale:
+        vmin, vmax = img.min(), img.max()
+        img = (img - vmin) / (vmax - vmin)
+    return np.clip(255 * img, 0.0, 255.0).astype(np.uint8)
 
 def image_from_url(url):
   """
